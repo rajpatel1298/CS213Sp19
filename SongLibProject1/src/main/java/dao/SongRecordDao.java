ea68e2deed08b1e331bfb089d373269e5f90eb94
Ian Morenot <imorenodev@gmail.com>
2019-02-08 17:50:26 -0500

    ian - initial commit, project structure Dao, Dto, db skeletons

---
+++ SongLibProject1/src/main/java/dao/SongRecordDao.java
@@ -0,0 +1,40 @@
+package main.java.dao;
+
+import java.util.List;
+import java.util.Optional;
+
+import main.java.models.SongRecordDto;
+
+public class SongRecordDao implements Dao<SongRecordDto> {
+
+	@Override
+	public Optional<SongRecordDto> get(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<SongRecordDto> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void save(SongRecordDto SongRecordDto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(SongRecordDto SongRecordDto, String[] params) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(SongRecordDto SongRecordDto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
