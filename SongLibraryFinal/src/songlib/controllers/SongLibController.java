8726e2f3bf0b71b42d3a4b1698b11d9d0187fe28
Ian Morenot <imorenodev@gmail.com>
2019-02-10 20:51:23 -0500

    added the song details in the SongLibView for whichever song is selected

--- SongLibraryFinal/src/songlib/controllers/SongLibController.java
+++ SongLibraryFinal/src/songlib/controllers/SongLibController.java
@@ -16,9 +16,11 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.ListView;
+import javafx.scene.text.Text;
 import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
 import songlib.dao.SongRecordDao;
+import songlib.models.SongRecordDto;
 
 public class SongLibController {
 	
@@ -26,6 +28,7 @@ public class SongLibController {
 	@FXML private Button addButton;
 	@FXML private Button editButton;
 	@FXML private Button deleteButton;
+	@FXML private Text songDetailText;
 	
 	public static int selectedRecordIndex;
 	private ObservableList<String> obsList;
@@ -45,6 +48,33 @@ public class SongLibController {
 		songList.getSelectionModel().select(selectedRecordIndex);
 		songList.getFocusModel().focus(selectedRecordIndex);
 		
+		if (selectedRecordIndex >= 0 && SongRecordDao.getInstance().getSortedSongList().length > 0) {
+			SongRecordDto song = SongRecordDao.getInstance().getSongRecord(selectedRecordIndex);
+			StringBuilder detailsSB = new StringBuilder();
+			if (song.getTitle() != null && !song.getTitle().isEmpty()) {
+				detailsSB.append("Title: ");
+				detailsSB.append(song.getTitle());
+				detailsSB.append("\n");
+			}
+			if (song.getAlbum() != null && !song.getAlbum().isEmpty()) {
+				detailsSB.append("Album: ");
+				detailsSB.append(song.getAlbum());
+				detailsSB.append("\n");
+			}
+			if (song.getArtist() != null && !song.getArtist().isEmpty()) {
+				detailsSB.append("Artist: ");
+				detailsSB.append(song.getArtist());
+				detailsSB.append("\n");
+			}
+			if (song.getYear() != null) {
+				detailsSB.append("Year: ");
+				detailsSB.append(song.getYear());
+			}	
+			songDetailText.setText(detailsSB.toString());
+		} else {
+			songDetailText.setText("Title:\nAlbum:\nArtist:\nYear:");
+		}
+		
 		// we want to disable the edit button if there are no records in the list
 		if (SongRecordDao.getInstance().getSortedSongList().length == 0) {
 			editButton.setDisable(true);
@@ -56,6 +86,32 @@ public class SongLibController {
 			@Override
 			public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
 				selectedRecordIndex = songList.getSelectionModel().getSelectedIndex();
+				if (selectedRecordIndex >= 0 && SongRecordDao.getInstance().getSortedSongList().length > 0) {
+					SongRecordDto song = SongRecordDao.getInstance().getSongRecord(selectedRecordIndex);
+					StringBuilder detailsSB = new StringBuilder();
+					if (song.getTitle() != null && !song.getTitle().isEmpty()) {
+						detailsSB.append("Title: ");
+						detailsSB.append(song.getTitle());
+						detailsSB.append("\n");
+					}
+					if (song.getAlbum() != null && !song.getAlbum().isEmpty()) {
+						detailsSB.append("Album: ");
+						detailsSB.append(song.getAlbum());
+						detailsSB.append("\n");
+					}
+					if (song.getArtist() != null && !song.getArtist().isEmpty()) {
+						detailsSB.append("Artist: ");
+						detailsSB.append(song.getArtist());
+						detailsSB.append("\n");
+					}
+					if (song.getYear() != null) {
+						detailsSB.append("Year: ");
+						detailsSB.append(song.getYear());
+					}	
+					songDetailText.setText(detailsSB.toString());
+				} else {
+					songDetailText.setText("Title:\nAlbum:\nArtist:\nYear:");
+				}
 			}
 		});
 	}
